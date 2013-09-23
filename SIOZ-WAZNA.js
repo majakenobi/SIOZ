5882cee30272fd30615df4f1fb37148ab8270b6f
majakenobi <maju17@poczta.onet.pl>
2013-09-23 15:12:27 +0200

    let it be

---
+++ SIOZ-WAZNA.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        SIOZ-WAZNA
+// @version     0.1
+// @copyright   2013+, Maja
+// ==/UserScript==
+
+$(document).ready(function() 
+{
+	var waznyField = $('input[name=wazny]');
+	waznyField.attr('checked', true);
+});
