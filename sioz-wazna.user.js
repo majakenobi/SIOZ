772e5b962c81949427eeeb62c66a3db0d51e1a1f
majakenobi <maju17@poczta.onet.pl>
2013-09-24 09:08:32 +0200

    include pages

--- sioz-wazna.user.js
+++ sioz-wazna.user.js
@@ -2,6 +2,8 @@
 // @name        SIOZ-WAZNA
 // @version     0.1
 // @namespace   http://sioz.softnet.pl/
+// @include     https://sioz.softnet.com.pl/firma/notka_w.php*
+// @include		https://sioz.softnet.com.pl/firma/main.php*
 // @copyright   2013+, Maja
 // ==/UserScript==
 
